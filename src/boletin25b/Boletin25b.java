1c26a73cf430f9f3f0cfef2417b2509a53e4b5bf
galle <galle@DESKTOP-J6RSNG6>
2016-06-04 16:30:53 +0200

    - Terminado

---
+++ src/boletin25b/Boletin25b.java
@@ -0,0 +1,14 @@
+package boletin25b;
+
+/**
+ *
+ * @author gallegomanuel88
+ */
+public class Boletin25b {
+
+    public static void main(String[] args) {
+        Ventana llamarVentana = new Ventana ();
+        llamarVentana.ventana();
+    }
+    
+}
